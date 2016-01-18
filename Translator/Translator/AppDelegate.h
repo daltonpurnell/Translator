26846e4a60fc1c0cae3e9e0547f181e11ca282aa
daltonpurnell <daltonpurnell@live.com>
2016-01-18 09:29:21 -0700

    project created

---
+++ Translator/Translator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Translator
+//
+//  Created by Dalton on 1/18/16.
+//  Copyright © 2016 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
